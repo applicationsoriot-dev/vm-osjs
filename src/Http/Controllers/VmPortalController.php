b21d77194fe2503de274b6450264d781791e574e
applicationsoriot-dev <application.soriot@gmail.com>
2026-02-22 11:06:35 +0100

    Update VmPortalController.php

--- src/Http/Controllers/VmPortalController.php
+++ src/Http/Controllers/VmPortalController.php
@@ -6,6 +6,7 @@ namespace Geodit\VmOsjs\Http\Controllers;
 
 use Geodit\VmOsjs\Support\VmStateResolver;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Illuminate\View\View;
 
 class VmPortalController
@@ -13,9 +14,28 @@ class VmPortalController
     public function __invoke(Request $request, VmStateResolver $resolver): View
     {
         $state = $resolver->resolve($request);
+        $embedRoute = Route::has('osjs.embed')
+            ? 'osjs.embed'
+            : (string) config('vm_osjs.route_names.embed', 'vm.osjs.embed');
+
+        $embedQuery = [
+            'stat' => (string) ($state['stat'] ?? 'guest'),
+        ];
+
+        foreach (['tenant', 'app'] as $key) {
+            $value = $request->query($key);
+            if ($value !== null && $value !== '') {
+                $embedQuery[$key] = $value;
+            }
+        }
+
+        $ctx = $request->query('ctx');
+        if (is_array($ctx) && $ctx !== []) {
+            $embedQuery['ctx'] = $ctx;
+        }
 
         return view('vm-osjs::portal', [
-            'embedUrl' => route(config('vm_osjs.route_names.embed', 'vm.osjs.embed'), ['stage' => $state['stage']]),
+            'embedUrl' => route($embedRoute, $embedQuery),
             'state' => $state,
         ]);
     }
